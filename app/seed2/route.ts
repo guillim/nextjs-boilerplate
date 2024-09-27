191da7375d28d182c852bd1d4b9b2b1d7d145096
Guillim <guillim@users.noreply.github.com>
2024-09-27 11:36:46 +0200

    Create user route

---
+++ app/seed2/route.ts
@@ -0,0 +1,16 @@
+import { CreateUser } from '../user/use-case';
+
+export async function GET() {
+  try {
+    await new CreateUser().createUser({
+      name: 'John Doe',
+      email: 'test2@test.test',
+      password: 'password',
+    });
+
+    return new Response(JSON.stringify({ message: 'Users created successfuly' }), { status: 200 });
+  } catch (error) {
+    console.error('Error during seeding:', error);
+    return new Response(JSON.stringify({ error }), { status: 500 });
+  }
+}
