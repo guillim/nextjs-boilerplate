8eae9524f5f8572fd18f241c2059d08c5e5f1900
Guillim <guillim@users.noreply.github.com>
2024-10-01 14:37:55 +0200

    Linting

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,5 @@
-import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '@/app/lib/data';
-import { GetUser, GetUserByEmail } from '../../user/use-case';
+import { GetUserByEmail } from '../../user/use-case';
  
 export default async function Page() {
   // const revenue = await fetchRevenue();
