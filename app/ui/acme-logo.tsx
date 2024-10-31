c5698381ea8029a2279ea1bd5ae55e1e32e97037
Guillim <guillim@users.noreply.github.com>
2024-10-31 11:12:47 +0100

    ACME logo link update

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,19 +1,22 @@
 import Image from 'next/image';
 import Logo from '../(landing-page)/public/images/logo.svg';
+import Link from 'next/link';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={'flex flex-row items-center leading-none text-white'}
-    >
-      <Image
-        src={Logo}
-        alt="Logo"
-        width={30}
-        height={30}
-        className="h-8 bg-white rounded-full max-w-fit"
-      />
-      <p className="text-[20px] text-black font-bold tracking-tight	pl-2">Acme</p>
-    </div>
+    <Link href="/">
+      <div
+        className={'flex flex-row items-center leading-none text-white'}
+      >
+        <Image
+          src={Logo}
+          alt="Logo"
+          width={30}
+          height={30}
+          className="h-8 bg-white rounded-full max-w-fit"
+        />
+        <p className="text-[20px] text-black font-bold tracking-tight	pl-2">Acme</p>
+      </div>
+    </Link>
   );
 }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
@@ -9,14 +8,13 @@ export default async function SideNav() {
   const name = user?.user?.name || user?.user?.email;
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
+      <span
         className="mb-2 flex h-10 items-end justify-start rounded-md p-2"
-        href="/dashboard"
       >
         <div className="w-32 text-black md:w-40">
           <AcmeLogo />
         </div>
-      </Link>
+      </span>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
