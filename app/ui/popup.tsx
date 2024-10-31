9d28f22191992d0cfea47241929235bf0ad6ede4
Guillim <guillim@users.noreply.github.com>
2024-10-31 11:23:47 +0100

    New component: Popup

---
+++ app/ui/popup.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { useState } from 'react'
+import { Button } from '../ui/button'
+import { InformationCircleIcon } from '@heroicons/react/24/outline'
+
+export default function Popup(
+  {msg, hideIcon = false, title, btnText, btnCloseText = 'Close', className = ''}:
+  {msg: string, hideIcon?: boolean, title: string, btnText: string , btnCloseText?: string, className?: string}) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <span className={"max-w-2xl margin-auto text-gray-500 text-sm "+className}
+        onClick={() => setIsOpen(true)}
+      >
+        {hideIcon &&
+        <InformationCircleIcon className={"h-4 w-4 text-gray-500 mr-1"} />
+        }
+        {btnText}
+      </span>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+            <DialogTitle className="font-bold">{title}
+            </DialogTitle>
+            <Description>{msg}</Description>
+            <div className="flex gap-4">
+              <Button onClick={() => setIsOpen(false)}>{btnCloseText}</Button>
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  )
+}
